1746f486cd89efb4c12a2b99170be00203fc38b0
Aristocrat <crataristo4@gmail.com>
2020-09-11 08:09:24 +0000

    compressed images uploaded ReportActivity.java

--- app/src/main/java/com/dalilu/ui/activities/ReportActivity.java
+++ app/src/main/java/com/dalilu/ui/activities/ReportActivity.java
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.media.MediaRecorder;
 import android.net.Uri;
 import android.os.Bundle;
@@ -40,7 +41,9 @@ import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -206,39 +209,78 @@ public class ReportActivity extends AppCompatActivity {
                 }).show();
     }
 
-    private void uploadToServer(Uri imageUri, String type) {
+    private void uploadToServer(Uri imageUri, String type) throws IOException {
         pd.show();
         StringBuilder address = new StringBuilder();
         address.append(knownName).append(",").append(state).append(",").append(country);
         @SuppressLint("SimpleDateFormat") DateFormat dateFormat = new SimpleDateFormat("EEE, d MMM yyyy HH:MM a");
         String dateReported = dateFormat.format(Calendar.getInstance().getTime());
 
+        //compress image
+        Bitmap bitmap;
+        bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), imageUri);
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 75, byteArrayOutputStream);
+
+        byte[] fileInBytes = byteArrayOutputStream.toByteArray();
         //upload photo to server
-        filePath.putFile(imageUri).continueWithTask(task -> {
+        filePath.putBytes(fileInBytes).continueWithTask(task -> {
+
             if (!task.isSuccessful()) {
                 pd.dismiss();
 
             }
             return filePath.getDownloadUrl();
 
+        }).addOnSuccessListener(this, o -> {
+            pd.dismiss();
+
+            Uri downLoadUri = Uri.parse(o.toString());
+            assert downLoadUri != null;
+            String url = downLoadUri.toString();
+
+            Map<String, Object> alertItems = new HashMap<>();
+            alertItems.put("userName", userName);
+            alertItems.put("userPhotoUrl", userPhotoUrl);
+            alertItems.put("url", url);
+            alertItems.put(type, type);
+            alertItems.put("coordinates", new GeoPoint(latitude, longitude));
+            alertItems.put("address", address.toString());
+            alertItems.put("userId", userId);
+            alertItems.put("phoneNumber", phoneNumber);
+            alertItems.put("timeStamp", GetTimeAgo.getTimeInMillis());
+            alertItems.put("dateReported", dateReported);
+
+            alertCollectionReference.add(alertItems);
+            String id = MainActivity.userId;
+
+            DisplayViewUI.displayToast(ReportActivity.this, getString(R.string.reportSuccess));
+
+            startActivity(new Intent(ReportActivity.this, MainActivity.class)
+                    .putExtra(AppConstants.UID, id)
+                    .putExtra(AppConstants.USER_NAME, userName)
+                    .putExtra(AppConstants.USER_PHOTO_URL, userPhotoUrl)
+                    .putExtra(AppConstants.PHONE_NUMBER, phoneNumber)
+
+
+            );
+            finish();
+
+
+        }).addOnFailureListener(this, e -> {
+            pd.dismiss();
+            DisplayViewUI.displayToast(this, Objects.requireNonNull(e.getMessage()));
+
+        });
+        
+
+
+       /* filePath.putFile(imageUri).continueWithTask(task -> {
+
+
         }).addOnCompleteListener(task -> {
             if (task.isSuccessful()) {
 
-                Uri downLoadUri = task.getResult();
-                assert downLoadUri != null;
-                String url = downLoadUri.toString();
-
-                Map<String, Object> alertItems = new HashMap<>();
-                alertItems.put("userName", userName);
-                alertItems.put("userPhotoUrl", userPhotoUrl);
-                alertItems.put("url", url);
-                alertItems.put(type, type);
-                alertItems.put("coordinates", new GeoPoint(latitude, longitude));
-                alertItems.put("address", address.toString());
-                alertItems.put("userId", userId);
-                alertItems.put("phoneNumber", phoneNumber);
-                alertItems.put("timeStamp", GetTimeAgo.getTimeInMillis());
-                alertItems.put("dateReported", dateReported);
 
 
                 //fire store cloud store
@@ -276,7 +318,7 @@ public class ReportActivity extends AppCompatActivity {
             }
 
         });
-
+*/
 
     }
 
@@ -303,7 +345,11 @@ public class ReportActivity extends AppCompatActivity {
                     pd = DisplayViewUI.displayProgress(ReportActivity.this, getString(R.string.uploadingImage));
 
 //upload to server
-                    uploadToServer(uri, "image");
+                    try {
+                        uploadToServer(uri, "image");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 });
 
 
@@ -335,7 +381,11 @@ public class ReportActivity extends AppCompatActivity {
                     pd = DisplayViewUI.displayProgress(ReportActivity.this, getString(R.string.uploadingVideo));
 
                     //upload to server
-                    uploadToServer(uri, "video");
+                    try {
+                        uploadToServer(uri, "video");
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
 
                 });
 
